f858e3b2af69b00180a536e90009e392a461a26b
Jongwook (John) Whangbo <jwwhangbo@gmail.com>
2023-10-31 18:25:00 -0400

    added password hashing when registering account. Login should work as well

--- mvc-backend/src/Controllers/UserController.php
+++ mvc-backend/src/Controllers/UserController.php
@@ -10,10 +10,35 @@ class UserController extends BaseController
     /**
      * Creates new user session from login credentials
      *
-     * @return void
+     * @param $request
+     * @return bool
      */
-    function create() {
-
+    function create($request): bool {
+        $userModel = new UserModel();
+        $username_request = array('username'=>$request['username']);
+        $password_request = $request['password'];
+        try {
+            $rows = $userModel->retrieve($username_request);
+            $user_result = $rows[0];
+            if (count($user_result) > 0) {
+                $user_password = $user_result[1];
+                if (password_verify($password_request, $user_password)) {
+                    echo true;
+                    return true;
+                } else {
+                    echo false;
+                    return false;
+                }
+            }
+            else {
+                echo false;
+                return false;
+            }
+        } catch (\Exception $e) {
+            http_response_code(500);
+            echo $e;
+            return false;
+        }
     }
 
     /**
@@ -23,17 +48,18 @@ class UserController extends BaseController
      */
     function store($request): bool
     {
+        # Hash password
         $userModel = new UserModel();
-        $request_username = $request['username'];
-        # validate $request_username
-        $validated = array();
-        if ($userModel->create($request)) {
-            # return some success case (depends on how to handle back- to frontend communication)
-            return true;
-        }
-        else {
-            # return some failure case
+        $request['password'] = password_hash($request['password'], PASSWORD_DEFAULT);
+        try {
+            $userModel->create($request);
+        } catch (\Exception $e) {
+            http_response_code(500);
+            echo $e;
             return false;
+        } finally {
+            echo true;
+            return true;
         }
     }
 }
